0c034e086b185ecf148725964f916dfc3d9b8558
Jorge CP <64277389+jp2435@users.noreply.github.com>
2023-07-20 23:14:39 +0100

    Update main.ps1
    Param BrightnessReset

--- main.ps1
+++ main.ps1
@@ -1,17 +1,36 @@
 param (
-    [int]$Brightness = 5  # Parâmetro opcional para o valor de brilho (padrão é 5)
+    [int]$Brightness = 5,        # Parâmetro opcional para o valor de brilho (padrão é 5)
+    [switch]$BrightnessReset     # Parâmetro opcional para redefinir o brilho para um valor específico
 )
 
-# Obtém o valor atual do brilho do monitor
-$Monitor = Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightness
-$currentBrightness = $Monitor.CurrentBrightness
+# Função para obter o valor atual do brilho do monitor
+function Get-Brightness {
+    Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightness | ForEach-Object { $_.CurrentBrightness }
+}
 
-# Se o parâmetro de brilho for enviado, adiciona o valor do parâmetro ao brilho atual
-$newBrightness = $currentBrightness + $Brightness
+# Função para ajustar o brilho do monitor para um valor específico
+function Set-Brightness {
+    param (
+        [int]$NewBrightness  # O valor de brilho a ser ajustado
+    )
+    $MonitorMethods = Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods
+    $MonitorMethods.WmiSetBrightness(1, $NewBrightness)
+}
 
-# Verifica se o novo valor está dentro do intervalo válido (0 a 100)
-$newBrightness = [Math]::Min([Math]::Max($newBrightness, 0), 100)
+# Obter o valor atual do brilho do monitor
+$currentBrightness = Get-Brightness
 
-# Ajusta o brilho para o novo valor incrementado
-$MonitorMethods = Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods
-$MonitorMethods.WmiSetBrightness(1, $newBrightness)
+# Se o parâmetro "BrightnessReset" for usado, definir o brilho para o valor especificado
+if ($BrightnessReset) {
+    Set-Brightness $Brightness
+}
+else {
+    # Se o parâmetro de brilho for enviado, adiciona o valor do parâmetro ao brilho atual
+    $newBrightness = $currentBrightness + $Brightness
+
+    # Verifica se o novo valor está dentro do intervalo válido (0 a 100)
+    $newBrightness = [Math]::Min([Math]::Max($newBrightness, 0), 100)
+
+    # Ajusta o brilho para o novo valor incrementado
+    Set-Brightness $newBrightness
+}
